848cdb3928ed0a4d50c89ba5fc037f8580b50021
RENAN CARDOSO PORTELA DOS SANTOS <232.933594@senaicimatec.edu.br>
2024-03-12 16:21:24 -0300

    Somavetor

---
+++ somavetor/src/App.java
@@ -0,0 +1,24 @@
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int vetor[] = {1,2,3,4};
+        
+
+        int soma = 0;
+
+
+        for(int i = 0; i<vetor.length; i++){
+           soma += vetor[i];
+        }
+        
+        
+        System.out.println(soma);
+    
+    
+    
+    }
+}
